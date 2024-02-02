3869a953780c98b4adad169b26fb46f3bf68a9fc
PATMESH <101879714+PATMESH@users.noreply.github.com>
2024-02-02 22:09:14 +0530

    updated

--- script.js
+++ script.js
@@ -32,3 +32,8 @@ const animationPath = 'lottie.json';
     checkScroll();
     window.addEventListener("scroll", checkScroll);
   });
+
+  document.addEventListener('mousemove', (e) => {
+    const cursor = document.querySelector('.cursor');
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  });
